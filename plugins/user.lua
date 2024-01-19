a7f74c16d62055f93c4835dc42b462201249c907
AlexBuchko <abuchko@calpoly.edu>
2024-01-19 02:29:21 -0800

    leap and spider

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,6 +2,7 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
+  { 'dccsillag/magma-nvim', run = ':UpdateRemotePlugins' }
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
