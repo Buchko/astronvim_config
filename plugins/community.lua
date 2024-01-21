a16427ca8c330ab397d60089bfd0b54873a246ec
Buchko <alexcbuchko@gmail.com>
2024-01-20 21:04:56 -0800

    foo

--- plugins/community.lua
+++ plugins/community.lua
@@ -9,4 +9,5 @@ return {
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.nvim-spider" },
+  { import = "astrocommunity.pack.cpp" },
 }
