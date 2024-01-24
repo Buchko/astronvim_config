b16e4c66273a3b3dfc4827ee63ddcd9fea602d14
Buchko <alexcbuchko@gmail.com>
2024-01-24 09:59:04 -0800

    hiii

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,9 +5,11 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.motion.nvim-spider" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
 }
